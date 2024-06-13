7b3512ac0a34645086dc9119869820af2924ee86
SchrimpsMitReis <roman.schroeder@inclufilm.com>
2024-06-13 05:39:21 +0200

    13.06

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { Component } from '@angular/core';
 import { LandingPageComponent } from './landing-page/landingPage.component';
 import { HowTwoComponent } from './how-two/how-two.component';
 import { OurMenuComponent } from './our-menu/our-menu.component';
+import { FindUsComponent } from './find-us/find-us.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, HowTwoComponent, OurMenuComponent],
+  imports: [CommonModule, LandingPageComponent, HowTwoComponent, OurMenuComponent, FindUsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
